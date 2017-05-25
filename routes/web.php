320cf9cba89def73bde9cf136fc629fb215e8e32
Rodrigo dos Santos Ferreira <Rodrigo@MacBook-Air-de-Rodrigo.local>
2017-05-25 04:39:56 -0300

    no message

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('champions', 'ChampionController@index')->name('champions');
+Route::get('/champions', 'ChampionController@index')->name('champions');
