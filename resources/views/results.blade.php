71221aad2e9109f30fa3db3f480ac615a708eae4
Rodrigo dos Santos Ferreira <Rodrigo@MacBook-Air-de-Rodrigo.local>
2017-05-30 10:31:37 -0300

    Correção para leitura de parágrafo

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <h2>Resultado da composição</h2>
-        <h2>{{$content}}</h2>
+        <p>{!! nl2br(e($content)) !!}</p>
 
     </div>
 @endsection
