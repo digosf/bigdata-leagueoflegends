e353ce8177013e222276f970d3161675f311c436
Rodrigo dos Santos Ferreira <Rodrigo@MacBook-Air-de-Rodrigo.local>
2017-06-27 02:37:35 -0300

    View e Controlador Consertado

--- app/Http/Controllers/ChampionController.php
+++ app/Http/Controllers/ChampionController.php
@@ -14,20 +14,33 @@ class ChampionController extends Controller
     public function store(Request $request) {
         $champions = $request->get('champions');
         $fileName = 'champions_' . uniqidReal() . '.json';
-        //$absolutePath = storage_path($fileName);
+        $absolutePath = storage_path($fileName);
 
-        //Storage::disk('local')->put($fileName, json_encode($champions));
+        Storage::disk('local')->put($fileName, json_encode($champions));
 
-        //$output = shell_exec('/opt/spark-2.1.1-bin-hadoop2.7/bin/spark-submit /var/www/html/projeto-bigdata/project/lolapp_2.11-1.0.jar /var/www/html/projeto-bigdata/storage/app/' . $fileName . ' 2>&1');
-        //Storage::disk('local')->put('output.txt', $output);
-		//Storage::disk('local')->delete($fileName);
+        $output = shell_exec('/opt/spark-2.1.1-bin-hadoop2.7/bin/spark-submit /var/www/html/projeto-bigdata/project/loldatafinder_2.11-1.0.jar /var/www/html/projeto-bigdata/storage/app/' . $fileName . ' 2>&1');
+        Storage::disk('local')->put($filename, $output);
 
         return json_encode(['data' => ['filename' => $fileName]]);
       }
 
     public function results ($id) {
-        //$content = Storage::disk('local')->get('results.txt');
-        //return view ('results', ['content' => $content]);
-        return view('results');
+       $apiLink = 'https://global.api.riotgames.com/api/lol/static-data/BR/v1.2/champion?champData=image&dataById=true&api_key=RGAPI-f08f49ff-27fb-4389-b413-fe17dc9d8370';
+       $imageLink = 'http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/';
+       $champion_data = json_decode(file_get_contents($apiLink), true);
+
+        $fileName = 'app/' . $id .'.json';
+        $absolutePath = storage_path($fileName);
+        $json_data = json_decode(file_get_contents($absolutePath), true);
+
+        for ($i =0; $i< count($json_data); $i ++){
+          for ($j = 0; $j <5; $j ++) {
+            $content[$i]['champions'][$j]['champion_name'] = $champion_data['data'][$json_data[$i][$j]]['name'];
+            $content[$i]['champions'][$j]['champion_image'] = $imageLink . $champion_data['data'][$json_data[$i][$j]]['image']['full'];
+          }
+            $content[$i]['victory_value'] = $json_data[$i][$j];
+        }
+        //dd($content[1]['champions']);
+        return view ('results', ['content' => $content]);
     }
 }
--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -2,78 +2,21 @@
 
 @section('content')
     <div class="container">
-        <!-- COMP 1 -->
+        <!-- COMP  -->
+        @foreach ($content as $compositions)
         <div class="row result-comp-row">
+          @foreach($compositions['champions'] as $key => $champion)
             <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 result-champ-col">
                 <div class="result-champ-box">
-                    <div class="title">Annie</div>
-                    <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Annie.png" class="img-responsive img-circle" alt="">
+                    <div class="title">{{$champion['champion_name']}}</div>
+                    <img src={{$champion['champion_image']}} class="img-responsive img-circle" alt="">
                 </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 result-champ-col">
-                <div class="result-champ-box">
-                    <div class="title">Annie</div>
-                    <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Annie.png" class="img-responsive img-circle" alt="">
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 result-champ-col">
-                <div class="result-champ-box">
-                    <div class="title">Annie</div>
-                    <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Annie.png" class="img-responsive img-circle" alt="">
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 result-champ-col">
-                <div class="result-champ-box">
-                    <div class="title">Annie</div>
-                    <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Annie.png" class="img-responsive img-circle" alt="">
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 result-champ-col">
-                <div class="result-champ-box">
-                    <div class="title">Annie</div>
-                    <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Annie.png" class="img-responsive img-circle" alt="">
-                </div>
-            </div>
+          @endforeach
             <div class="win-rate-box">
-                <p><span class="rate-label bg-green">75%</span> de vitórias</p>
+               <p><span class="rate-label bg-green">{{$compositions['victory_value']}}</span> vitórias</p>
             </div>
         </div>
-
-        <!-- COMP 2 -->
-        <div class="row result-comp-row">
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 result-champ-col">
-                <div class="result-champ-box">
-                    <div class="title">Annie</div>
-                    <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Annie.png" class="img-responsive img-circle" alt="">
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 result-champ-col">
-                <div class="result-champ-box">
-                    <div class="title">Annie</div>
-                    <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Annie.png" class="img-responsive img-circle" alt="">
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 result-champ-col">
-                <div class="result-champ-box">
-                    <div class="title">Annie</div>
-                    <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Annie.png" class="img-responsive img-circle" alt="">
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 result-champ-col">
-                <div class="result-champ-box">
-                    <div class="title">Annie</div>
-                    <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Annie.png" class="img-responsive img-circle" alt="">
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 result-champ-col">
-                <div class="result-champ-box">
-                    <div class="title">Annie</div>
-                    <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Annie.png" class="img-responsive img-circle" alt="">
-                </div>
-            </div>
-            <div class="win-rate-box">
-                <p><span class="rate-label bg-red">75%</span> de vitórias</p>
-            </div>
-        </div>
+        @endforeach
     </div>
 @endsection
